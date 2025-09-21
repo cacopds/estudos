4215ca46961a86213babf40292d366f11bddabfa
cacopds <cacopds@gmail.com>
2025-09-20 23:24:09 -0300

    Primeiro envio

---
+++ meu-primeiro-projeto/apps/src/test/java/br/com/casa/apps/AppsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casa.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
